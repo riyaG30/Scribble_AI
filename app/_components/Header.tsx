6a86943884670d1b17b9c7c01e906ec97cc6e8d6
Riya-Gupta3010 <riya.gupta3010@ibm.com>
2025-02-25 21:21:44 +0530

    removed image issue

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,16 +1,11 @@
 import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
-import Image from "next/image";
+
 
 function Header() {
     return (
         <header className="bg-black">
             <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-                <Image
-                    src="/scribble_verse/public/logo-black.png"
-                    alt="logo"
-                    width={100}
-                    height={100}
-                />
+               
 
                 <div className="flex flex-1 items-center justify-end md:justify-between">
                     <nav aria-label="Global" className="hidden md:block">
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ["lh3.googleusercontent.com"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+            },
+        ],
     },
     env: {
         WATSONX_API_KEY: process.env.WATSONX_API_KEY, // Expose API key
